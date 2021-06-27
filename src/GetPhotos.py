4ac739a968017b93e36f51d1e2c19b45c48473eb
Justin Ho <paladiamors@yahoo.com>
2021-06-27 21:28:32 +0900

    Minor update

--- src/GetPhotos.py
+++ src/GetPhotos.py
@@ -5,43 +5,54 @@ import datetime
 from exif import Image
 from functools import reduce
 
+# using cable to connect to iphone is unreliable -- better to just sync to a server and then
+# categorize the photos after
 sourceDirectories = [  # '/home/justin/.gvfs/Justin \xe3\x81\xae iPhone/DCIM/100APPLE',
     # '/home/justin/.gvfs/Justin \xe3\x81\xae iPhone/DCIM/101APPLE',
-    '/media/justin/NEW VOLUME',
-    '/media/justin/FC30-3DA9',
-    '/home/justin/temp/PhotoDump',
-    '/home/justin/Drives/2.3TB/downloads/photos']
+    # '/media/justin/NEW VOLUME',
+    # '/home/justin/temp/PhotoDump',
+    '/home/justin/ownCloud/IphoneMedia',
+    ]
 
 mediaDirectory = "/home/justin/Drives/2.3TB/Pictures"
 
 
 def exif_date(path):
-	"""
-	Given the path of a file returns the exif datetime of a file
-	"""
+    """
+    Returns the exif date of an image file
+    """
 
-    with open(path, 'rb') as image_file:
+    # gets the exif data for jpg files
+    ext = path.split(".")[-1].lower()
+    if ext != "jpg":
+        return False
+
+    with open(path, "rb") as image_file:
         my_image = Image(image_file)
 
     if my_image.has_exif:
-        date = datetime.datetime.strptime(my_image.datetime, "%Y:%m:%d %H:%M:%S")
-		print("exif date", path, date)
+        try:
+            date = datetime.datetime.strptime(my_image.datetime, "%Y:%m:%d %H:%M:%S")
+            print("exif date", path, date)
+        except AttributeError:
+            print("attribute error on", path)
+            return False
         return date
-    
+
     return False
+
 
 def getDate(file):
     "returns the date of creation for a file in yyyy-mm-dd"
+    year = 0
+    month = 1
+    day = 2
 
-	date = exif_date(file)
-	if not date:
-    	date = time.strptime(time.ctime(os.path.getmtime(file)))
+    date = exif_date(file)
 
-    # some code to deal with photos in a different time zone:
-#	date = datetime.datetime(date[year], date[month], date[day], date[hour], date[minute])
-#	timedelta = datetime.timedelta(0,16*60*60) #16 hours for Canada
-#	date = date - timedelta
-#	date = (date.year, date.month, date.day)
+    if not date:
+        date = time.strptime(time.ctime(os.path.getmtime(file)))
+        date = datetime.date(date[year], date[month], date[day])
 
     dateStr = date.strftime("%Y_%m_%d")
     return dateStr
@@ -58,9 +69,9 @@ def getMediaFileList(path):
     """returns a list of media files in a path,returns a list of JPG and MOV files"""
 
     fileTypes = ("jpg", "mov", "mp4")
-    dirList = ["/".join([path, object]) for object in os.listdir(path)]
-    fileList = [object for object in dirList if os.path.isfile(object)]
-    fileList = [file for file in fileList if file.split(".")[1].lower() in fileTypes]
+    fileList = []
+    for base_dir, dirs, files in os.walk(path):
+        fileList.extend([os.path.join(base_dir, f) for f in files if f.split(".")[1].lower() in fileTypes])
 
     # for the new canon camera, ther are some .Trash and trashinfo files, want to ignore them
     fileList = [file for file in fileList if "trash" not in file and "Trash" not in file]
@@ -75,12 +86,12 @@ def getMountedMediaDirectories():
 def getMediaFiles(path):
     """function to get list of JPG and .mov files, recursively checks paths"""
     fileList = getMediaFileList(path)
-    dirList = getDirectoryList(path)
+    # dirList = getDirectoryList(path)
 
-    results = map(getMediaFiles, dirList)
+    # results = map(getMediaFiles, dirList)
 
-    for result in results:
-        fileList = fileList + result
+    # for result in results:
+    #     fileList = fileList + result
 
     return fileList
 
@@ -103,6 +114,7 @@ def copyMedia(source, target):
 if __name__ == "__main__":
     # getting list of files
     directories = getMountedMediaDirectories()
+
     def combine(x, y): return x + y
     mediaFileList = reduce(combine, map(getMediaFiles, directories))
 
@@ -122,6 +134,7 @@ if __name__ == "__main__":
     for source, target in zip(mediaFileList, copyTargetList):
         try:
             copyMedia(source, target)
+
         except OSError:
             errors.append(source)
             print("error transferring", source)
