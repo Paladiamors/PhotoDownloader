46a3c3c21520af71043754e8170256d6c4ba773f
Justin Ho <paladiamors@yahoo.com>
2023-02-05 01:22:21 +0900

    adding conversion script for heic files

---
+++ src/convert.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Code is modelled based on https://askubuntu.com/questions/62492/how-can-i-change-the-date-modified-created-of-a-file
+
+for file in *.HEIC; do 
+  echo "Converting $file to ${file%%.HEIC}.jpg"
+  heif-convert "$file" ${file%%.HEIC}.jpg
+
+  echo "reading original creation date"
+  date=$(stat -c %y -t "$file"| cut -d "." -f 1)
+
+  echo "writing original creation date as modification and creation date"
+  touch -am --date="$date" ${file%%.HEIC}.jpg
+done
